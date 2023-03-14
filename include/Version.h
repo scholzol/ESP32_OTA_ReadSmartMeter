cb13380aaf21f4413cf429f1a552181c0b6d8378
olaf scholz <olaf.scholz@online.de>
2023-03-14 10:17:16 +0100

    version

--- include/Version.h
+++ include/Version.h
@@ -1,4 +1,4 @@
-#define SemanticVersion "0.1.2"
-#define SHA_short "219e1f2"
-#define FullBuildMetaData "46.Branch.master.Sha.219e1f2646d7589bcf5f6e5a06cc2ff0f0d27afd"
-#define WorkingDirectory "ESP32_OTA_Template"
+#define SemanticVersion "0.1.0"
+#define SHA_short "69f9bfd"
+#define FullBuildMetaData "2.Branch.master.Sha.69f9bfdbc459487e019f66a7d4ba37ad4c5fa76e"
+#define WorkingDirectory "ESP32_OTA_ReadSmartMeter"
